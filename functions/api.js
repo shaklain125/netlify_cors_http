37fbe429dfcffdefb87c9b5d11eab37b617e9806
shaklain <48572696+shaklain125@users.noreply.github.com>
2021-11-24 04:09:45 +0000

    test resp 2
    Signed-off-by: shaklain <48572696+shaklain125@users.noreply.github.com>

--- functions/api.js
+++ functions/api.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const serverless = require("serverless-http");
-const { httpRequest } = require("./utils");
+// const { httpRequest } = require("./utils");
 
 const app = express();
 const router = express.Router();
@@ -8,19 +8,19 @@ const router = express.Router();
 router.get("/", (req, res) => {
 	const { url, ...rest } = req.query;
 	return res.send({ url, rest });
-	if (url) {
-		httpRequest(url, rest).then(({ data, req_options, headers }) => {
-			Object.entries(headers).forEach(([key, value]) => {
-				if (key.match(/encoding/gi)) return;
-				res.setHeader(key, value);
-			});
-			res.setHeader("req_params", JSON.stringify(req_options));
-			res.send(data);
-		});
-		return;
-	}
-	res.set("Content-Type", "text/html");
-	res.send("");
+	// if (url) {
+	// 	httpRequest(url, rest).then(({ data, req_options, headers }) => {
+	// 		Object.entries(headers).forEach(([key, value]) => {
+	// 			if (key.match(/encoding/gi)) return;
+	// 			res.setHeader(key, value);
+	// 		});
+	// 		res.setHeader("req_params", JSON.stringify(req_options));
+	// 		res.send(data);
+	// 	});
+	// 	return;
+	// }
+	// res.set("Content-Type", "text/html");
+	// res.send("");
 });
 
 app.use(`/.netlify/functions/api`, router);
