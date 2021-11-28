6a835acee5ff47e9cfb214b156f87619897cf4ae
shaklain <48572696+shaklain125@users.noreply.github.com>
2021-11-28 05:59:23 +0000

    cache fix 1
    Signed-off-by: shaklain <48572696+shaklain125@users.noreply.github.com>

--- functions/api.js
+++ functions/api.js
@@ -50,7 +50,42 @@ router.get("/", (req, res) => {
 
 router.get("/text", (req, res) => {
 	const { url, ...rest } = req.query;
-	res.send({ url, ...rest });
+	if (url) {
+		httpRequest(url, {
+			...rest,
+			responseType: "arraybuffer",
+		}).then(({ data, req_options, headers }) => {
+			let cors_headers = {
+				"Access-Control-Allow-Headers":
+					"Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin",
+				"Access-Control-Allow-Origin": "*",
+				Vary: "Origin",
+			};
+			Object.entries(cors_headers).forEach(([key, value]) => {
+				res.setHeader(key, value);
+			});
+			res.setHeader("req_params", JSON.stringify(req_options));
+			const ctype = getContentType(headers);
+			const is_img_ctype = isImageContentType(ctype);
+			const is_unknown = is_unknown_file(data);
+			res.send({ url, ...rest, is_unknown });
+			return;
+			// Object.entries(headers).forEach(([key, value]) => {
+			// 	if (key.match(/encoding/gi)) return;
+			// 	res.setHeader(key, value);
+			// });
+			// if (is_img_ctype) {
+			// 	res.send(data.toString("base64"));
+			// } else if (is_unknown) {
+			// 	res.send(data);
+			// } else {
+			// 	res.send(data.toString());
+			// }
+		});
+		return;
+	}
+	res.set("Content-Type", "text/html");
+	res.send("");
 });
 
 app.use(`/.netlify/functions/api`, router);
